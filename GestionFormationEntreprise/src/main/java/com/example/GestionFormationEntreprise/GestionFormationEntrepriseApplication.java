6628af68dd25331f2d76f35e348f21f19a87514c
tina <tinhinaneachir@gmail.com>
2025-01-03 15:42:01 +0100

    create CRUD  and controller employee

---
+++ GestionFormationEntreprise/src/main/java/com/example/GestionFormationEntreprise/GestionFormationEntrepriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.GestionFormationEntreprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionFormationEntrepriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionFormationEntrepriseApplication.class, args);
+	}
+
+}
---
+++ GestionFormationEntreprise/src/test/java/com/example/GestionFormationEntreprise/GestionFormationEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GestionFormationEntreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionFormationEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
